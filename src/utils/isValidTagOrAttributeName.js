ebe76dd765cfbbfb4ca8c0d79c6754d9d9069e5b
Yoshihiro OKUMURA <orrisroot@gmail.com>
2022-05-26 11:12:17 +0900

    Fixed: should filter out as invalid attributes. reference: https://github.com/peternewnham/react-html-parser/pull/90

--- src/utils/isValidTagOrAttributeName.js
+++ src/utils/isValidTagOrAttributeName.js
@@ -3,6 +3,10 @@ const VALID_TAG_REGEX = /^[a-zA-Z][a-zA-Z:_.\-\d]*$/;
 const nameCache = {};
 
 export default function isValidTagOrAttributeName(tagName) {
+  if(tagName === 'constructor') {
+    return false;
+  }
+
   if (!Object.prototype.hasOwnProperty.call(nameCache, tagName)) {
     nameCache[tagName] = VALID_TAG_REGEX.test(tagName);
   }
